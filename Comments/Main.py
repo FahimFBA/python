cad0beab8c084405fa7fef0d2916bf87983dd9dc
FahimFBA <fahimbinamin@gmail.com>
2023-10-12 02:20:32 +0600

    comment

---
+++ Comments/Main.py
@@ -0,0 +1,23 @@
+# This is a single line comment
+
+"""This is also a valid comment"""
+
+"""
+    This
+    is
+    a
+    multi
+    line 
+    comment
+"""
+
+print("I am learning comments")
+
+'''
+This
+is
+another
+example
+of multi line
+comments
+'''
