ff9a1d2e7370be715555cdab81bde3b498492531
FahimFBA <fahimbinamin@gmail.com>
2023-10-23 01:39:44 +0600

    variables

---
+++ Variables/Main.py
@@ -0,0 +1,24 @@
+# a = 40
+# b = 15
+
+# print(a)
+# print(b)
+
+
+# x = "Fahim" # string
+# print(x)
+
+# y = 'Fahim'
+# print(y)
+
+
+# x = int(100)
+# print(type(x))
+
+# y = str(100)
+# print(type(y))
+
+Y = 20
+print(Y)
+y = 25
+print(y)
