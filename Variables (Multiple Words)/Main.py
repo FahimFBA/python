3a1b00ba9393c58f64a62de6b0c171d6829d1c29
FahimFBA <fahimbinamin@gmail.com>
2023-10-27 02:22:52 +0600

    var. name

---
+++ Variables (Multiple Words)/Main.py
@@ -0,0 +1,22 @@
+# School Name
+
+# SchoolName
+# schoolName
+# School_Name
+
+
+# My School Name
+
+# Pascal Case
+# Every word's first letter is an uppercase letter
+MySchoolName = "UIU"
+
+# Camel Case
+# each word, except the first word, always starts with a capital letter
+mySchoolName = "BRAC"
+
+
+# Snake Case
+# Each word is separated by an underscore character
+My_School_Name = "AIUB"
+my_school_name = "BUET"
