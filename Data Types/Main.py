ed0c232442e0046b4522c05eeb6ac80ce67dd9aa
FahimFBA <fahimbinamin@gmail.com>
2023-11-08 01:35:45 +0600

    data type

---
+++ Data Types/Main.py
@@ -0,0 +1,46 @@
+# Text type = String = str
+a = 'this is an string'
+print(a)
+a = "this is an string"
+print(a)
+
+
+# Numeric type = Numbers
+
+# int
+x = 54
+print(x)
+
+# float
+y = 45.89
+print(y)
+
+# complex
+
+# Boolean = true/false
+z = True
+print(z)
+
+# sequence type
+
+# list
+# tuple
+# range
+
+# mapping type
+
+# dict
+
+# set types
+
+# set
+# frozenset
+
+# binary type
+
+# bytes
+# bytearray
+# memoryview
+
+# None type
+# noneType
