a87856538a0f98acea5dfaf19ed767e31407a150
FahimFBA <fahimbinamin@gmail.com>
2023-12-12 01:12:48 +0600

    random

---
+++ Random Number/Main.py
@@ -0,0 +1,8 @@
+# Built in module named random
+
+
+# importing the built in module
+import random
+
+# print random numbers with range
+print(random.randrange(1, 100))
---
+++ Random Number/Main2.py
@@ -0,0 +1,8 @@
+# importing the built in module
+import random
+
+# print 10 random numbers with range
+i = 0
+while(i < 10):
+    print(random.randrange(1, 100))
+    i = i + 1
