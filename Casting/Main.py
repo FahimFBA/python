d1486e2b6463a108fb4073c3b2bda60853146372
FahimFBA <fahimbinamin@gmail.com>
2023-12-12 14:09:11 +0600

    casting

---
+++ Casting/Main.py
@@ -0,0 +1,29 @@
+# a = 1
+# print(a)
+
+# a = int(5.6)
+# print(a)
+
+# a = int('679')
+# print(a)
+# print(type(a))
+
+# a = int('679gh')
+# print(a)
+# print(type(a))
+
+# a = float(2)
+# print(a)
+# print(type(a))
+
+a = str(23423)
+print(a)
+print(type(a))
+
+b = str("234982346jhbasdj")
+print(b)
+print(type(b))
+
+c = str(5.765756)
+print(c)
+print(type(c))
